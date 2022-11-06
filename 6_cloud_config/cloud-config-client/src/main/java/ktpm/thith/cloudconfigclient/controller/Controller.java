a113ab7f75293eb28566798a369b6d83a6724809
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-11-07 00:45:37 +0700

    6 cloud config

---
+++ 6_cloud_config/cloud-config-client/src/main/java/ktpm/thith/cloudconfigclient/controller/Controller.java
@@ -0,0 +1,19 @@
+package ktpm.thith.cloudconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class Controller {
+
+    @Value("${server.port}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return message;
+    }
+}
